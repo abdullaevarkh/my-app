c69d42bdda85a1ee28b292d3af747f4a61c55df3
user <abullayeva.ar96@gmail.com>
2023-02-05 13:36:38 +0600

    part one

--- src/App.js
+++ src/App.js
@@ -6,11 +6,11 @@ function App() {
   return (
     <div className="menu_block_head">
     <div className="menu_block">
-      <a href="/home" className='menu_links'>Home</a>
-      <a href="/about_me" className='menu_links'>About me</a>
-      <a href="/skills" className='menu_links'>Skills</a>
-      <a href="/portfolio" className='menu_links'>Portfolio</a>
-      <a href="/contacts" className='menu_links'>Contacts</a>
+      <a href="/home" className='menu_links1'>Home</a>
+      <a href="/about_me" className='menu_links1'>About me</a>
+      <a href="/skills" className='menu_links1'>Skills</a>
+      <a href="/portfolio" className='menu_links1'>Portfolio</a>
+      <a href="/contacts" className='menu_links1'>Contacts</a>
     </div>
     </div>
   );
