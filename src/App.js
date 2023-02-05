4111bad97f88b2299ce7015c0850fa8bee1745c8
user <abullayeva.ar96@gmail.com>
2023-02-05 17:19:06 +0600

    save project1

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import qwerty from './qwerty.jpg';
+import sloy from './sloy.png';
+import reebok_web from './reebok_web.png';
+import reebok_w from './reebok_w.png';
 import './App.css';
 
 function App() {
@@ -44,15 +47,33 @@ function App() {
         </div>
       </div>
     </div>
-<div className="menu_block_head4">
+    <div className="menu_block_head4">
+        <h4 className="skills">Skills</h4>
+        </div>
+<div className="menu_block_head5">
   <div>
-    <h1>Skills</h1>
+    <h1 className="portfolio">Portfolio</h1>
   </div>
-  <div>
-    <h1>I work in such programs as</h1>
+<div className="content_img2">
+    <img src={sloy} alt={"sloy"}/> 
+    </div>
+    <div className="margig-top48">
+      <a href="fashion_storage_hp" className="fashion_storage_hp">Fashion store - Homepage</a>
   </div>
-</div>
-    </>
+<div className="content_img3">
+    <img src={reebok_web} alt={"reebook_web"}/> 
+    </div>
+    <div  className="margig-top48">
+    <a href="rs_concept" className="rs_concept">Reebok Store - Concept</a>
+    </div>
+    <div className="content_img4">
+      <img src={reebok_w} alt={"reebook_w"}/> 
+    </div>
+    <div  className="margig-top48">
+    <a href="blp_concept" className="blp_concept">Braun Loading Page - Concept</a>
+    </div>
+ </div>
+</>
   );
 }
 
